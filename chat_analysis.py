7c3a42bc415e1e918423532584bb8afde6d03a6b
Umang-19 <umangaggarwal537@gmail.com>
2022-07-15 17:00:54 +0530

    Results

---
+++ chat_analysis.py
@@ -0,0 +1,87 @@
+import re
+import pandas as pd
+import numpy as np
+import emoji
+from collections import Counter
+import matplotlib.pyplot as plt
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import warnings
+warnings.filterwarnings('ignore')
+
+# Extract the Date time
+def date_time(s):
+    pattern='^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]? -'
+    result=re.match(pattern, s)
+    if result:
+        return True
+    return False 
+
+# Extract contacts
+def find_contact(s):
+    s=s.split(":")
+    if len(s)==2:
+        return True
+    else:
+        return False
+
+# Extract Message
+def getMassage(line):
+    splitline=line.split(' - ')
+    datetime= splitline[0];
+    date, time= datetime.split(', ')
+    message=" ".join(splitline[1:])
+    
+    if find_contact(message):
+        splitmessage=message.split(": ")
+        author=splitmessage[0]
+        message=splitmessage[1]
+    else:
+        author=None
+    return date, time, author, message
+
+data=[]
+conversation='../input/chatdata/chatdata.txt'
+with open(conversation, encoding="utf-8") as fp:
+    fp.readline()
+    messageBuffer=[]
+    date, time, author= None, None, None
+    while True:
+        line=fp.readline()
+        if not line:
+            break
+        line=line.strip()
+        if date_time(line):
+            if len(messageBuffer) >0:
+                data.append([date, time, author, ''.join(messageBuffer)])
+            messageBuffer.clear()
+            date, time, author, message=getMassage(line)
+            messageBuffer.append(message)
+        else:
+            messageBuffer.append(line)
+        
+df=pd.DataFrame(data, columns=["Date", "Time", "contact", "Message"])
+df['Date']=pd.to_datetime(df['Date'])
+
+data=df.dropna()
+
+sentiments=SentimentIntensityAnalyzer()
+data["positive"]=[sentiments.polarity_scores(i)["pos"] for i in data["Message"]]
+data["negative"]=[sentiments.polarity_scores(i)["neg"] for i in data["Message"]]
+data["neutral"]=[sentiments.polarity_scores(i)["neu"] for i in data["Message"]]
+
+data.head()
+
+x = sum(data["positive"])
+y = sum(data["negative"])
+z = sum(data["neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😡")
+    else:
+        print("Neutral 😐 ")
+sentiment_score(x, y, z)
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/feelprivacy').then(()=>{
+mongoose.connect('mongodb://localhost:27017/detectemotion').then(()=>{
     console.log(`Connection with Database Successfull!`);
 }).catch((err)=>{
     console.log(err);
